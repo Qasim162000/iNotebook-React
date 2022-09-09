e8ab8cfbea25b8fac9014d621c6e863fe6ad1ba5
Qasim <m.qasim162000@gmail.com>
2022-09-09 16:42:21 +0500

    At backend: Changed port from 3000 to 5000 since we'd be running react frontend on 3000, and better the authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
-// Create a User using: POST "/api/auth". Doesn't require auth
+// Create a User using: POST "/api/auth/createuser". No Login Required
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a Valid Name").isLength({ min: 3 }),
     body("email", "Enter a Valid Email").isEmail(),
@@ -13,24 +13,32 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    //If errors exist, return bad request and the error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: "Please enter a unique value for email",
-          message: err.message,
-        });
+    // Check whether the user with this email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      //Create a new User
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
       });
+
+      res.json(user);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Some error occured");
+    }
   }
 );
 
